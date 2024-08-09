d2dcf139a56b918dfced5d0460342bf4b95db574
Moon Knight <103847581+Twilight-Killer@users.noreply.github.com>
2024-08-09 22:17:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -193,16 +193,6 @@ async def _(event):
         )
 
 
-@BotzHub.on(events.NewMessage(pattern="^/start$"))
-async def strt(event):
-    await event.reply(
-        f"Hi. I'm a force subscribe bot made specially for @{channel}!\n\nCheckout @BotzHub :)",
-        buttons=[
-            Button.url("Channel", url=f"https://t.me/{channel}"),
-            Button.url("Repository", url="https://github.com/xditya/ForceSub"),
-        ],
-    )
 
-
-log.info("ForceSub Bot has started as @%s.\nDo visit @BotzHub!", bot_self.username)
+log.info("ForceSub Bot has started as @%s.", bot_self.username)
 BotzHub.run_until_disconnected()
